3388872e97f784c33467b9c976d3b3301c7a1788
Prfedo <esmailmohamedali27@gmail.com>
2026-04-13 15:35:15 +0200

    Add files via upload

---
+++ BookstoreApp/src/com/bookstore/controller/OrderController.java
@@ -0,0 +1,37 @@
+package com.bookstore.controller;
+
+import com.bookstore.model.CartItem;
+import com.bookstore.model.Order;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderController {
+    private List<Order> orders;
+
+    public OrderController() {
+        orders = new ArrayList<>();
+    }
+
+    // Place a new order
+    public Order placeOrder(int userId, List<CartItem> cartItems) {
+        String date = LocalDate.now().toString();
+        Order order = new Order(orders.size() + 1, userId, cartItems, date);
+        orders.add(order);
+        return order;
+    }
+
+    // Get orders by user
+    public List<Order> getOrdersByUser(int userId) {
+        List<Order> result = new ArrayList<>();
+        for (Order o : orders) {
+            if (o.getUserId() == userId) result.add(o);
+        }
+        return result;
+    }
+
+    // Get all orders (admin)
+    public List<Order> getAllOrders() {
+        return orders;
+    }
+}
