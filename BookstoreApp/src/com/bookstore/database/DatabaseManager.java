7f6683172e57a18be1845bbe16d1564cb2fd53c8
Malak Medhat <eng.malakms@gmail.com>
2026-04-13 19:14:35 +0200

    Testing

--- BookstoreApp/src/com/bookstore/database/DatabaseManager.java
+++ BookstoreApp/src/com/bookstore/database/DatabaseManager.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.bookstore.database;
 
-/**
- *
- * @author Admin
- */
 public class DatabaseManager {
     private static final String URL = "jdbc:sqlite:bookstore.db";
 
---
+++ BookstoreApp/src/com/bookstore/database/testing.java
@@ -0,0 +1,7 @@
+package com.bookstore.database;
+
+public class testing {
+    //i Love you habibaaaaaaaaaaaaaa
+    int a;
+    String Habiba;
+}
