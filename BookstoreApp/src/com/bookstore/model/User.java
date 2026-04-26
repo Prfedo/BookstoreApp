c1186f6f2c304bfc178a9ae006dcd151de39d52b
Malak Medhat <eng.malakms@gmail.com>
2026-04-26 09:55:43 +0300

    Add username field to User class

--- BookstoreApp/src/com/bookstore/model/User.java
+++ BookstoreApp/src/com/bookstore/model/User.java
@@ -1,23 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.bookstore.model;
 
-/**
- *
- * @author Admin
- */
 public class User {
     private int id;
     private String name;
+    private String username;
     private String email;
     private String password;
     private boolean isAdmin;
 
-    public User(int id, String name, String email, String password, boolean isAdmin) {
+    public User(int id, String name,String username, String email, String password, boolean isAdmin) {
         this.id = id;
         this.name = name;
+        this.username=username;
         this.email = email;
         this.password = password;
         this.isAdmin = isAdmin;
@@ -25,11 +19,13 @@ public class User {
 
     public int getId()          { return id; }
     public String getName()     { return name; }
+    public String getUsername() { return username; }
     public String getEmail()    { return email; }
     public String getPassword() { return password; }
     public boolean isAdmin()    { return isAdmin; }
 
     public void setName(String name)       { this.name = name; }
+    public void setUsername(String username) { this.username = username; }
     public void setEmail(String email)     { this.email = email; }
     public void setPassword(String pass)   { this.password = pass; }
     public void setAdmin(boolean isAdmin)  { this.isAdmin = isAdmin; }
